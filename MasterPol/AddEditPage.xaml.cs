87a808ce716bb475ec9568def5dbe543e4e46eff
jamid <jamid@DESKTOP-E8C6K79>
2025-04-17 13:40:20 +0500

    Добавьте файлы проекта.

---
+++ MasterPol/AddEditPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPol
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        int k = 0;
+        private Partners _currentAgent = new Partners();
+        public AddEditPage(Partners SelectedPartners)
+        {
+            InitializeComponent();
+            if (SelectedPartners != null)
+            {
+                _currentAgent = SelectedPartners;
+                k++;
+            }
+            DataContext = _currentAgent;
+          
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if(string.IsNullOrWhiteSpace(_currentAgent.PartnerName))
+            {
+                errors.AppendLine("jfdfk");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.JuridicalIndex))
+            {
+                errors.AppendLine("Укажите индекс");
+            }
+            if (!(_currentAgent.JuridicalIndex.Length == 6))
+            {
+                errors.AppendLine("Укажите правильный индекс");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.JuridicalState))
+            {
+                errors.AppendLine("Укажите субъект");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.JuridicalCity))
+            {
+                errors.AppendLine("Укажите город");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.JuridicalStreet))
+            {
+                errors.AppendLine("Укажите улицу");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.JuridicalHome.ToString()))
+            {
+                errors.AppendLine("Укажите номер дома");
+            }
+            if (_currentAgent.JuridicalHome <= 0)
+            {
+                errors.AppendLine("Укажите положительный номер дома");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.DirectorName))
+            {
+                errors.AppendLine("Укажите директора");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.INN))
+            {
+                errors.AppendLine("Укажите ИНН");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.Rate.ToString()))
+            {
+                errors.AppendLine("Укажите приоритет");
+            }
+            if (CB_Sel.SelectedItem == null)
+            {
+                errors.AppendLine("Укажите тип агента");
+            }
+            if (_currentAgent.Rate <= 0)
+                errors.AppendLine("Укажите положительный приоритет");
+            if (string.IsNullOrWhiteSpace(_currentAgent.Email))
+            {
+                errors.AppendLine("Укажите Email");
+            }
+            else if (!_currentAgent.Email.Contains('@') && !_currentAgent.Email.Contains('.'))
+            {
+                errors.AppendLine("Укажите почту правильно");
+            }
+            if (string.IsNullOrWhiteSpace(_currentAgent.Phone))
+            {
+                errors.AppendLine("Укажите телефон");
+            }
+            else
+            {
+                string ph = _currentAgent.Phone;
+                if (((ph[1] == '9' || ph[1] == '4' || ph[1] == '8') && ph.Length != 11) || ph[1] == '3' && ph.Length != 12)
+                    errors.AppendLine("Введите телефон правильно");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            _currentAgent.PartnerType = CB_Sel.SelectedIndex + 1;
+            if(_currentAgent.ID == 0)
+            {
+                YamaletdinovMasterPolEntities.GetContext().Partners.Add(_currentAgent);
+            }
+            try
+            {
+                YamaletdinovMasterPolEntities.GetContext().SaveChanges();
+                MessageBox.Show("Saves success");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ MasterPol/PartnerPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPol
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class PartnerPage : Page
+    {
+        public PartnerPage()
+        {
+            InitializeComponent();
+            Partner_LW.ItemsSource = YamaletdinovMasterPolEntities.GetContext().Partners.ToList();
+            pt_cb.SelectedIndex = 0;
+            Update_Agent();
+        }
+        public void Update_Agent()
+        {
+            var current_Agent = YamaletdinovMasterPolEntities.GetContext().Partners.ToList();
+
+            if(RadioButton_Up.IsChecked == true)
+            {
+                current_Agent = current_Agent.OrderBy(p => p.PartnerName).ToList();
+            }
+            if (RadioButton_Down.IsChecked == true)
+            {
+                current_Agent = current_Agent.OrderByDescending(p => p.PartnerName).ToList();
+            }
+            current_Agent = current_Agent.Where(p => p.PartnerName.ToLower().Contains(SerachTB.Text.ToLower()) || p.Email.ToLower().Contains(SerachTB.Text.ToLower()) || p.PhoneFiltr.Contains(SerachTB.Text.Replace("+", "").Replace("-", "").Replace("(", "").Replace(")", "").Replace(" ", ""))).ToList();
+            if (pt_cb.SelectedIndex == 1)
+            {
+                current_Agent = current_Agent.Where(p => p.PartnerType == 1).ToList();
+            }
+            if (pt_cb.SelectedIndex == 2)
+            {
+                current_Agent = current_Agent.Where(p => p.PartnerType == 2).ToList();
+            }
+            if (pt_cb.SelectedIndex == 3)
+            {
+                current_Agent = current_Agent.Where(p => p.PartnerType == 3).ToList();
+            }
+            if (pt_cb.SelectedIndex == 4)
+            {
+                current_Agent = current_Agent.Where(p => p.PartnerType == 4).ToList();
+            }
+
+            Partner_LW.ItemsSource = current_Agent;
+        }
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Partners));
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void SerachTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update_Agent();
+        }
+
+        private void RadioButton_Up_Checked(object sender, RoutedEventArgs e)
+        {
+            Update_Agent();
+        }
+
+        private void RadioButton_Down_Checked(object sender, RoutedEventArgs e)
+        {
+            Update_Agent();
+        }
+
+        private void pt_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update_Agent();
+        }
+    }
+}
